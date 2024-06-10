c96f6f8b678ba1b7643c0902e93a96591e5feb70
anhkiet2030 <dinhanhkiet2003@gmail.com>
2024-06-10 20:14:00 +0700

    newCommit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Gửi tặng';
+const titleCard = 'Tặng Em';
+const contentCard = 'Chúc em buổi tối ấm áp, anh viết thư này gửi tặng em, cảm ơn em đã đến đây với anh và quan tâm anh, ở bên cạnh em anh cảm thấy bản thân vui và hạnh phúc lắm Love You!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'nhung.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
